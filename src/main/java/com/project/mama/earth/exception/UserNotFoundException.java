c8d1e51daad137942c363e58672e4680af98f54a
sakshyagarg <104513669+sakshyagarg@users.noreply.github.com>
2022-06-26 17:10:19 +0530

    Delete src/main/java/com/project/mama/earth/exception directory

--- src/main/java/com/project/mama/earth/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.mama.earth.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
