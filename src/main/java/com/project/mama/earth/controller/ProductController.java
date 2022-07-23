1601ff141a950ca555e1d4a7afdf49da77e3a573
sakshyagarg <104513669+sakshyagarg@users.noreply.github.com>
2022-07-23 21:10:03 +0530

    Update ProductController.java

--- src/main/java/com/project/mama/earth/controller/ProductController.java
+++ src/main/java/com/project/mama/earth/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/mamaearth")
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("*")
 
 public class ProductController {
     @Autowired
