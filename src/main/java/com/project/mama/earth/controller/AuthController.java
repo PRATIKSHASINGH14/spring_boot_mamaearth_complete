a8c8a0fc0a72fe9db0d24b0ea3039c9815cd9d95
sakshyagarg <104513669+sakshyagarg@users.noreply.github.com>
2022-07-23 21:09:17 +0530

    Update AuthController.java

--- src/main/java/com/project/mama/earth/controller/AuthController.java
+++ src/main/java/com/project/mama/earth/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("*")
 
 public class AuthController {
     @Autowired
